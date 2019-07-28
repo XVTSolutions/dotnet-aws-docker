d6bec532b762ac35ab4d2dfea4e012f075064b5f
Michael Southeren <michael.southeren@xvt.com.au>
2019-07-28 15:21:11 +1000

    Upgrade to .NET Core 2.2

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@
 
 # Based on the previous status this is not a for build, but runtime only.
 
-FROM microsoft/dotnet:2.1-aspnetcore-runtime
+FROM microsoft/dotnet:2.2-aspnetcore-runtime
 
 RUN apt-get update && apt-get -y upgrade && apt-get install -y awscli python3-pip && apt-get clean
 
--- update-dotnet.sh
+++ update-dotnet.sh
@@ -3,13 +3,13 @@
 # Run this script to bring up the last up-to-date version of the current m$
 # dotnet image and build the coresponding xvt ones.
 
-DOTNET_VERSION="2.1"
+DOTNET_VERSION="2.2"
 
 echo "Current dotnet version: $DOTNET_VERSION."
 
 docker pull microsoft/dotnet:${DOTNET_VERSION}-aspnetcore-runtime
 
-FULL_VERSION_TAG=$(docker run --rm microsoft/dotnet:2.1-aspnetcore-runtime dotnet --info | grep Version | awk '{print $2}')
+FULL_VERSION_TAG=$(docker run --rm microsoft/dotnet:2.2-aspnetcore-runtime dotnet --info | grep Version | awk '{print $2}')
 
 docker build -t xvtsolutions/dotnet-aspnetcore-runtime-aws:$FULL_VERSION_TAG -f Dockerfile.dotnet-aspnetcore-runtime-aws .
 docker tag xvtsolutions/dotnet-aspnetcore-runtime-aws:$FULL_VERSION_TAG xvtsolutions/dotnet-aspnetcore-runtime-aws:latest
@@ -29,8 +29,8 @@ docker tag xvtsolutions/dotnet-aspnetcore-runtime-aws-unoconv-newrelic:$FULL_VER
 
 # sonar scanner
 
-docker pull microsoft/dotnet:2.1-sdk
-FULL_VERSION_TAG=$(docker run --rm microsoft/dotnet:2.1-sdk dotnet --version)
+docker pull microsoft/dotnet:2.2-sdk
+FULL_VERSION_TAG=$(docker run --rm microsoft/dotnet:2.2-sdk dotnet --version)
 docker build -t xvtsolutions/dotnet-sdk-sonar-scanner:${FULL_VERSION_TAG} -f Dockerfile.sonar-scanner .
 
 echo

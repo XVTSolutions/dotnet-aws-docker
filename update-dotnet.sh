4c2e4e4994d437c685a89d725c0b0bacc6f3eb38
Gitea <gitea@fake.local>
2019-07-29 11:08:40 +1000

    Update the update script again and cleaning up

--- update-dotnet.sh
+++ update-dotnet.sh
@@ -11,24 +11,37 @@ docker pull microsoft/dotnet:${DOTNET_VERSION}-aspnetcore-runtime
 
 FULL_VERSION_TAG=$(docker run --rm microsoft/dotnet:2.2-aspnetcore-runtime dotnet --info | grep Version | awk '{print $2}')
 
+
 docker build -t xvtsolutions/dotnet-aspnetcore-runtime-aws:$FULL_VERSION_TAG -f Dockerfile.dotnet-aspnetcore-runtime-aws .
 docker tag xvtsolutions/dotnet-aspnetcore-runtime-aws:$FULL_VERSION_TAG xvtsolutions/dotnet-aspnetcore-runtime-aws:latest
+
+OUPUT_TEXT="$OUPUT_TEXT docker push xvtsolutions/dotnet-aspnetcore-runtime-aws:$FULL_VERSION_TAG\n"
 
 docker build -t xvtsolutions/dotnet-aspnetcore-runtime-aws-newrelic:$FULL_VERSION_TAG -f Dockerfile.dotnet-aspnetcore-runtime-aws-newrelic .
 docker tag xvtsolutions/dotnet-aspnetcore-runtime-aws-newrelic:$FULL_VERSION_TAG xvtsolutions/dotnet-aspnetcore-runtime-aws-newrelic:latest
 
+OUPUT_TEXT="$OUPUT_TEXT docker push xvtsolutions/dotnet-aspnetcore-runtime-aws-newrelic:$FULL_VERSION_TAG\n"
+
 docker build -t xvtsolutions/dotnet-aspnetcore-runtime-aws-unoconv:$FULL_VERSION_TAG -f Dockerfile.dotnet-aspnetcore-runtime-aws-unoconv .
 docker tag xvtsolutions/dotnet-aspnetcore-runtime-aws-unoconv:$FULL_VERSION_TAG xvtsolutions/dotnet-aspnetcore-runtime-aws-unoconv:latest
+
+OUPUT_TEXT="$OUPUT_TEXT docker push xvtsolutions/dotnet-aspnetcore-runtime-aws-unoconv:$FULL_VERSION_TAG\n"
 
 docker pull microsoft/dotnet:${DOTNET_VERSION}-runtime
 docker build -t xvtsolutions/dotnet-runtime-aws:${FULL_VERSION_TAG} -f Dockerfile.dotnet-console-runtime .
 docker tag xvtsolutions/dotnet-runtime-aws:${FULL_VERSION_TAG} xvtsolutions/dotnet-runtime-aws:latest
 
+OUPUT_TEXT="$OUPUT_TEXT docker push xvtsolutions/dotnet-runtime-aws:${FULL_VERSION_TAG}\n"
+
 docker build -t xvtsolutions/dotnet-runtime-aws-unoconv:$FULL_VERSION_TAG -f Dockerfile.runtime.unoconv .
 docker tag  xvtsolutions/dotnet-runtime-aws-unoconv:$FULL_VERSION_TAG  xvtsolutions/dotnet-runtime-aws-unoconv:latest
 
+OUPUT_TEXT="$OUPUT_TEXT docker push xvtsolutions/dotnet-runtime-aws-unoconv:$FULL_VERSION_TAG\n"
+
 docker build -t xvtsolutions/dotnet-aspnetcore-runtime-aws-unoconv-newrelic:$FULL_VERSION_TAG -f Dockerfile.dotnet-aspnetcore-runtime-aws-unoconv-newrelic .
 docker tag xvtsolutions/dotnet-aspnetcore-runtime-aws-unoconv-newrelic:$FULL_VERSION_TAG xvtsolutions/dotnet-aspnetcore-runtime-aws-unoconv-newrelic:latest
+
+OUPUT_TEXT="$OUPUT_TEXT docker push xvtsolutions/dotnet-aspnetcore-runtime-aws-unoconv-newrelic:$FULL_VERSION_TAG \n"
 
 # sonar scanner
 
@@ -36,16 +49,13 @@ docker pull microsoft/dotnet:2.2-sdk
 FULL_VERSION_TAG=$(docker run --rm microsoft/dotnet:2.2-sdk dotnet --version)
 docker build -t xvtsolutions/dotnet-sdk-sonar-scanner:${FULL_VERSION_TAG} -f Dockerfile.sonar-scanner .
 
+OUPUT_TEXT="$OUPUT_TEXT docker push xvtsolutions/dotnet-sdk-sonar-scanner:${FULL_VERSION_TAG}\n"
+
 echo
 echo "############"
 echo "Completed build for the following images:"
 echo
-echo "docker push xvtsolutions/dotnet-aspnetcore-runtime-aws:$FULL_VERSION_TAG"
-echo "docker push xvtsolutions/dotnet-aspnetcore-runtime-aws-newrelic:$FULL_VERSION_TAG"
-echo "docker push xvtsolutions/dotnet-aspnetcore-runtime-aws-unoconv:$FULL_VERSION_TAG"
-echo "docker push xvtsolutions/dotnet-aspnetcore-runtime-aws-unoconv-newrelic:$FULL_VERSION_TAG"
-echo "docker push xvtsolutions/dotnet-runtime-aws:${FULL_VERSION_TAG}"
-echo "docker push xvtsolutions/dotnet-runtime-aws-unoconv:$FULL_VERSION_TAG"
+printf "$OUPUT_TEXT"
 echo
 echo "You have to login docker hub and push it manually if it is ready"
 echo
